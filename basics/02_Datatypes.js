91bbca2da5ad68b6335a2978806dca4771c943c0
Hushcoder <130145629+Hushcoder@users.noreply.github.com>
2023-09-06 18:30:12 +0000

    variables

--- basics/02_Datatypes.js
+++ basics/02_Datatypes.js
@@ -9,15 +9,28 @@
 
 // Primitive Datatypes
 /*Many Datatypes but most used are
- number=>range 2 power 53
- bigint
- string 
- boolean, 
+ Number=>range 2 power 53 , float is counted as Number only here...
+ bigint  , put a n after any long number is read as bigint....like 3546925851628n
+ String 
+ Boolean, 
  null=> Standalone value(can be assigned as value)
  undefined
- symbol=> for uniqueness*/
+ Symbol=> for uniqueness*/
+ 
+ //Non - Primitive
+ /*Reference
+  javascript Objects[done],Array,Functions*/
 
- //Object
+  /*Array ----> const keda=['machhar','makkhi']   returntype => object
+  Object -----> let myobj={                       returntype => object 
+    name:'vishal'
+    age:20
+  }
+  Functions-----> const myfunc=function(){        returntype => object function                        
+          console.log("hello world")
+  }*/
+
+  
 
  console.log(typeof undefined); // returns type undefined
  console.log(typeof null); // returns object                  Important!!!!
