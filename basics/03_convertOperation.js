82abd71be42b764bc1faaab0ab59b5e72f11544e
Hushcoder <130145629+Hushcoder@users.noreply.github.com>
2023-09-06 17:23:16 +0000

    convert operation

--- basics/03_convertOperation.js
+++ basics/03_convertOperation.js
@@ -21,3 +21,30 @@ console.log(boolLogIn);
 //"" => false , " anything " => true
 
 //Same for all Datatypes
+
+/****************OPERATIONS******************/
+
+// let val=3
+// let negval=-val
+// console.log(negval);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/3)           ALL PERFORMS BASIC MATH OPERATIONS
+// console.log(2*4)
+// console.log(2**1)
+// console.log(2%10)
+
+/**********Tricky Ones *************/
+
+console.log(1+"2");   //gives 12
+console.log("1"+2);   //gives 12
+console.log("1"+2+3); //gives 123
+console.log(1+3+"2"); //gives 42
+
+//All rules are given in ECMA standards tc39.es
+
+//But no need to write this above type of messy code instead use parenthesis to establish precedence according to your choice.
+
+console.log(+true); //gives 1   no need to write like this except for understanding.
+console.log(+""); //gives 0
