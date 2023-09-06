7791ae5326a21f20d7fdd60d40c83e0e2a5079ba
Hushcoder <130145629+Hushcoder@users.noreply.github.com>
2023-09-06 16:11:52 +0000

    Datatypes

---
+++ basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treats all JS code as newer version as earlier JS didn't have modules,functions... but now it have.
+
+//alert(3+3)  In javascript, it will generate alert with 6 but here it give error as we're working in NodeJs not browser. 
+
+//Code Readibility should be high (neat code that's why use or no use ;)
+
+//For Documentation visit webdoc mdn or tc39(ECMA standards)-original doc
+
+
+// Primitive Datatypes
+/*Many Datatypes but most used are
+ number=>range 2 power 53
+ bigint
+ string 
+ boolean, 
+ null=> Standalone value(can be assigned as value)
+ undefined
+ symbol=> for uniqueness*/
+
+ //Object
+
+ console.log(typeof undefined); // returns type undefined
+ console.log(typeof null); // returns object                  Important!!!!
+
+
