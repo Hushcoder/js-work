a2e776fb8ff3577a216039a32d849e09d06a6610
Hushcoder <130145629+Hushcoder@users.noreply.github.com>
2023-09-06 16:38:56 +0000

    Datatype Conversion

---
+++ basics/03_convertOperation.js
@@ -0,0 +1,23 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN => prints Not a number but type is number.  !!! A little different
+//true => 1, false => 0
+
+let login= 1
+let boolLogIn= Boolean(login)
+console.log(typeof login);
+console.log(typeof boolLogIn);
+console.log(boolLogIn);
+
+//1 => true , 0 => false
+//"" => false , " anything " => true
+
+//Same for all Datatypes
