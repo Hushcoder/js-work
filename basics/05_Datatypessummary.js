5a3f8ab439eead7e9f089d93f8e51d9aa6c78ec1
Hushcoder <130145629+Hushcoder@users.noreply.github.com>
2023-09-06 18:27:06 +0000

    datatype summary

---
+++ basics/05_Datatypessummary.js
@@ -0,0 +1,22 @@
+// Primitive Datatypes
+/*Many Datatypes but most used are
+ Number=>range 2 power 53 , float is counted as Number only here...
+ bigint  , put a n after any long number is read as bigint....like 3546925851628n
+ String 
+ Boolean, 
+ null=> Standalone value(can be assigned as value)
+ undefined
+ Symbol=> for uniqueness*/
+ 
+ //Non - Primitive
+ /*Reference
+  javascript Objects[done],Array,Functions*/
+
+  /*Array ----> const keda=['machhar','makkhi']   returntype => object
+  Object -----> let myobj={                       returntype => object 
+    name:'vishal'
+    age:20
+  }
+  Functions-----> const myfunc=function(){        returntype => object function                        
+          console.log("hello world")
+  }*/
